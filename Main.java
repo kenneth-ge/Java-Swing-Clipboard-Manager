3e9cf2e23eaa8c71bfd98fba53068d5c446a4615
kennethge <57784063+kenneth-ge@users.noreply.github.com>
2023-01-25 14:45:38 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,49 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		JFrame frame = new JFrame("Clipboard");
+		
+		BufferedReader br = new BufferedReader(new FileReader("copy.txt"));
+		
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		
+		frame.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(3,3,3,3);
+		
+		String line;
+		while((line = br.readLine()) != null) {
+			String[] stuff = line.split("\t");
+			
+			JButton btn = new JButton(stuff[0]);
+			btn.addActionListener(e -> {
+				StringSelection selection = new StringSelection(stuff[1]);
+				clipboard.setContents(selection, selection);
+			});
+			
+			frame.add(btn, gbc);
+		}
+		
+		br.close();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setSize(300, 200);
+		frame.setVisible(true);
+	}
+
+}
